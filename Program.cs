3345b515b5e142993a01522c05ed88b4625e01f7
ffoegs <g.barlow@node4.co.uk>
2018-12-07 12:51:55 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,54 +1,41 @@
-using System;
-using System.Data.SqlClient;
-using System.Text;
+using Microsoft.AspNetCore.Builder;
 
-namespace sqltest
+using Microsoft.AspNetCore.Hosting;
+
+using System.IO;
+
+
+
+namespace TodoApi
+
 {
-    class Program
+
+    public class Program
+
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
+
         {
-            try 
-            { 
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "n4eventsqlserver.database.windows.net"; 
-                builder.UserID = "node4";            
-                builder.Password = "RedBus09";     
-                builder.InitialCatalog = "eventsqldb";
 
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-                {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
+            var host = new WebHostBuilder()
 
-                    connection.Open();       
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
-                    sb.Append("FROM [SalesLT].[ProductCategory] pc ");
-                    sb.Append("JOIN [SalesLT].[Product] p ");
-                    sb.Append("ON pc.productcategoryid = p.productcategoryid;");
-                    String sql = sb.ToString();
+                .UseKestrel()
 
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
-                    }                    
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.ReadLine();
+                .UseContentRoot(Directory.GetCurrentDirectory())
+
+                .UseIISIntegration()
+
+                .UseStartup<Startup>()
+
+                .Build();
+
+
+
+            host.Run();
+
         }
+
     }
+
 }
-
-
-
