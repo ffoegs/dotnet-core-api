c17f284006274227400c7d5591c4e64eb21fb6f9
ffoegs <g.barlow@node4.co.uk>
2018-12-07 12:24:20 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,6 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 using System.Text;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
-
-namespace TodoApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
-    }
-}
-
 
 namespace sqltest
 {
